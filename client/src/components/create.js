e163173038976a06866aabddd2c121090cc6b926
肖宇航 <xiaoyuhang@xiaoyuhangdeMacBook-Air.local>
2022-09-20 23:39:44 -0700

    Update

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -23,7 +23,7 @@ export default function Create() {
     // When a post request is sent to the create url, we'll add a new record to the database.
     const newPerson = { ...form };
 
-    await fetch("course/add", {
+    await fetch("/course/add", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -25,7 +25,7 @@ export default function RecordList() {
   // This method fetches the records from the database.
   useEffect(() => {
     async function getRecords() {
-      const response = await fetch(`course/`);
+      const response = await fetch(`/course/`);
 
       if (!response.ok) {
         const message = `An error occured: ${response.statusText}`;
@@ -44,7 +44,7 @@ export default function RecordList() {
 
   // This method will delete a record
   async function deleteRecord(id) {
-    await fetch(`${id}`, {
+    await fetch(`/${id}`, {
       method: "DELETE"
     });
 
