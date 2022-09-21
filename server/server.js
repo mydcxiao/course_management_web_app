8a9dc632d54541c5a400bd9abec716020f0e9101
肖宇航 <xiaoyuhang@xiaoyuhangdeMacBook-Air.local>
2022-09-20 23:27:47 -0700

    Update

--- server/server.js
+++ server/server.js
@@ -11,9 +11,9 @@ app.use(require("./routes/record"));
 const dbo = require("./db/conn");
 
 if(process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.use(express.static(path.resolve(__dirname, "../client/build")));
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
   });
 }
 
